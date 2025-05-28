2afb07b4fe1d66d3625d9f35ce76edc7629f6e9e
Aditi <ap6297630@gmail.com>
2025-05-29 01:07:06 +0530

    Initial commit for Render deployment

--- app.js
+++ app.js
@@ -107,6 +107,9 @@ app.use((err, req, res, next) => {
 });
 
 // Server start
-app.listen(8080, () => {
-  console.log("Server is listening on port 8080");
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
+
